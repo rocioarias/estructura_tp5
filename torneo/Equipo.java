c23d08a1e53eb11adb2f496b06fdeed87c79af26
Rocio A. Arias <rocioarias888@gmail.com>
2022-11-16 00:18:30 -0300

    Creacion de clase Equipo y agregarpartido.

--- torneo/Equipo.java
+++ torneo/Equipo.java
@@ -1,7 +1,56 @@
 package torneo;
 
-
 public class Equipo {
-    //TODO: Completar
+    
+    private char nombre;
+    private int golesContra;
+    private int golesAFavor;
+    private int puntos;
+    
+    public Equipo(int gC, int gF, int puntos, char nom) {
+        this.golesContra = gC;
+        this.golesAFavor = gF;
+        this.nombre = nom;
+        this.puntos = puntos;
+    }
 
+    public Equipo() {
+        this.golesContra = 0;
+        this.golesAFavor = 0;
+        this.puntos = 0;
+        this.nombre = "";
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    public int getGolesContra() {
+        return golesContra;
+    }
+    
+    public char getNombre() {
+        return nombre;
+    }
+
+    public void setNombre (char nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setGolesContra(int golesContra) {
+        this.golesContra = golesContra;
+    }
+
+    public int getGolesAFavor() {
+        return golesAFavor;
+    }
+
+    public void setGolesAFavor(int golesAFavor) {
+        this.golesAFavor = golesAFavor;
+    }
+   
 }
--- torneo/PartidosEquipo.java
+++ torneo/PartidosEquipo.java
@@ -1,5 +1,15 @@
 package torneo;
 
-public class PartidosEquipo {
-    //TODO: Completar.
+public class PartidosEquipo{
+    
+    class Nodo {
+        private Equipo dato;
+        private Nodo izquierda, derecha;
+
+        public Nodo(Equipo dato) {
+            this.dato = dato;
+            this.izquierda = this.derecha = null;
+        }
+    }
+    
 }
